47f566b3be99697fae90ddefbcd409a0548330d7
Jm Casler <jm@casler.org>
2015-07-12 23:07:32 -0700

    Updated defaults file.

--- src/yado/defaults.ino
+++ src/yado/defaults.ino
@@ -1,17 +1,25 @@
 void loadSettings ( void ) {
-  ssid = "changeme";
-  password = "changeme" ;
   
+  settings.ipAddress[0] = 10;
+  settings.ipAddress[1] = 11;
+  settings.ipAddress[2] = 12;
+  settings.ipAddress[3] = 36;
   
-  // Access password
-  access[0] = 'c';
-  access[1] = 'h';
-  access[2] = 'a';
-  access[3] = 'n';
-  access[4] = 'g';
-  access[5] = 'e';
-  access[6] = '!';
-  access[7] = '!';
+  settings.ipGateway[0] = 10;
+  settings.ipGateway[1] = 11;
+  settings.ipGateway[2] = 12;
+  settings.ipGateway[3] = 1;
   
-  useDHCP = false;
+  settings.ipSubnet[0] = 255;
+  settings.ipSubnet[1] = 255;
+  settings.ipSubnet[2] = 255;
+  settings.ipSubnet[3] = 0;
+  
+  settings.ipMode = 0; // 0 = Dynamic, 1 = Static
+  
+  strncpy(settings.ssid, "ssid_name_here", 32);
+  strncpy(settings.ssidPassword, "ssid_password_here", 64);
+  strncpy(settings.accessGeneral[0].password, "access_password", passwordLength);
+
+  requestTTL = 600;
 };
